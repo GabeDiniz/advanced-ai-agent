131be74f59dd3ab022bb4e86850e69ca8e22bf09
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-07-26 21:44:40 -0400

    [init] main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+
+llm = Ollama(
+    model="mistral",
+    request_timeout=30,
+)
+
+result = llm.complete("Hello, how are you?")
+print(result)
